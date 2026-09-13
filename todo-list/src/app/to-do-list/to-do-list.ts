316f26608295571111da6d831b44c437c0756d0d
Шаталов Максим <shatalov_mv@ascon.ru>
2026-09-13 20:35:02 +0300

    Рефакторинг Toasts

--- todo-list/src/app/to-do-list/to-do-list.ts
+++ todo-list/src/app/to-do-list/to-do-list.ts
@@ -1,13 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ToDoCreateItem } from '../to-do-create-item/to-do-create-item';
-import { ToastComponent } from "../toast-component/toast-component";
 import { ItemsFilter } from "../items-filter/items-filter";
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-to-do-list',
   imports: [ToDoCreateItem,
-    ToastComponent,
     ItemsFilter,
     RouterOutlet],
   templateUrl: './to-do-list.html',
